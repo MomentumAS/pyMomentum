daf1825f738049347a15d5222993ac53025b4267
tagmat <emir@tagmat.net>
2017-04-03 21:17:08 +0300

    First commit with basic python module structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyMomentum',
+      version='0.1',
+      description='Python libraries developed by Momentum',
+      url='https://github.com/MomentumAS/pyMomentum',
+      author='Momentum Teknoloji AS',
+      author_email='info@mtas.com.tr',
+      license='MIT',
+      packages=['pyMomentum'],
+      zip_safe=False
+      )
